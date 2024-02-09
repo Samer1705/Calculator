8492cb844c18868be973f6e83d71d3cd8fc7cccf
Samer1705 <samersameh175@gmail.com>
2024-02-09 22:20:58 +0200

    Created 'clearAll' Function

--- main.c
+++ main.c
@@ -9,20 +9,39 @@
 #include "keypad.h"
 #include "avr/delay.h"
 
-uint8 g_chars[16] = {0}, count = 1;
+float64 g_numsAndSigns[16] = {0}, result;
+uint8 g_chars[16] = {0}, g_count = 1;
 
+void clearAll()
+{
+	g_numsAndSigns[16] = 0;
+	g_chars[16] = 0;
+	LCD_clearScreen();
+	LCD_displayString("CALCULATOR:");
+	LCD_moveCursor(1, 0);
+}
 
 int main(void)
 {
 	uint8 key;
 	LCD_init();
 	LCD_displayString("CALCULATOR:");
-	LCD_moveCursor(1, 1);
+	LCD_moveCursor(1, 0);
 	while(1)
 	{
 		key = KEYPAD_getPressedKey();
-		if(key >= 0 && key <= 9) LCD_intgerToString(key);
-		else LCD_displayCharacter(key);
+		switch(key)
+		{
+		case 13:
+			clearAll();
+			break;
+		case '=':
+			break;
+		default:
+			if(key >= 0 && key <= 9) LCD_intgerToString(key);
+			else LCD_displayCharacter(key);
+			break;
+		}
 		_delay_ms(200);
 	}
 }
